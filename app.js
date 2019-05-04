e2e25edeb3d8b48e0fcdc02ef63d8d0953e11535
Claudiu Ungureanu <claudiu.ungureanu@bearingpoint.com>
2019-05-04 15:42:09 +0300

    connected to mongo atlas

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ path = require('path'),
 //favicon = require('serve-favicon'),
 bodyParser = require('body-parser'),
 cors = require('cors'),
+businessRoute = require('./routes/business.route');
+
 mongoose = require('mongoose'),
 config = require('./DB');
-
-businessRoute = require('./routes/business.route');
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
     () => { console.log('Database is connected') },
     err => { console.log('Can not connect to the database' + err) }
 );
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://ungureanu.claudiu.alexandru@gmail.com:Botosani1989!1@cluster0-t0frr.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../dist')));
@@ -19,7 +30,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/business', businessRoute);
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 const server = app.listen(port, function () {
     console.log('Listening on port ' + port);
