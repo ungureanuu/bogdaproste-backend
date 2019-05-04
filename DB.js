d27c7fe4e1545f2c11c70c2a0b01e54f6355cb84
Claudiu Ungureanu <claudiu.ungureanu@bearingpoint.com>
2019-05-04 14:59:04 +0300

    small changes in appjs

--- DB.js
+++ DB.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DB: 'mongodb+srv://ungureanu:Botosani1989!1@cluster0-t0frr.mongodb.net/test?retryWrites=true'
+    DB: 'mongodb://ungureanu:Botosani1989@ds050539.mlab.com:50539/bogdaprosteapp'
 };
 
 // mongodb://ungureanu:Botosani1989@ds050539.mlab.com:50539/bogdaprosteapp
--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express'),
-    path = require('path'),
-    //favicon = require('serve-favicon'),
-    bodyParser = require('body-parser'),
-    cors = require('cors'),
-    // mongoose = require('mongoose'),
-    // config = require('./DB');
+path = require('path'),
+//favicon = require('serve-favicon'),
+bodyParser = require('body-parser'),
+cors = require('cors'),
+mongoose = require('mongoose'),
+config = require('./DB');
 
-const businessRoute = require('./routes/business.route');
-// mongoose.Promise = global.Promise;
-// mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-//     () => { console.log('Database is connected') },
-//     err => { console.log('Can not connect to the database' + err) }
-// );
+businessRoute = require('./routes/business.route');
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => { console.log('Database is connected') },
+    err => { console.log('Can not connect to the database' + err) }
+);
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../dist')));
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/business', businessRoute);
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 const server = app.listen(port, function () {
     console.log('Listening on port ' + port);
