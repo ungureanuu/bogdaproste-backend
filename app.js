e62bafedb5ea10ae358e34ab2fe9d671f5b90a54
Claudiu Ungureanu <claudiu.ungureanu@bearingpoint.com>
2019-05-04 21:15:43 +0300

    implement get

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express'),
+const router = express.Router(),
+
 path = require('path'),
 //favicon = require('serve-favicon'),
 bodyParser = require('body-parser'),
@@ -13,23 +15,18 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
     err => { console.log('Can not connect to the database' + err) }
 );
 
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://ungureanu.claudiu.alexandru@gmail.com:Botosani1989!1@cluster0-t0frr.mongodb.net/test?retryWrites=true";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-
 const app = express();
 app.use(express.static(path.join(__dirname, '../dist')));
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/business', businessRoute);
+
+/* GET home page */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3] });
+});
+
 const port = process.env.PORT || 8080;
 
 const server = app.listen(port, function () {
