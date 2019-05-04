609d69642a1a760d4716bb34e242810f89991132
Claudiu Ungureanu <claudiu.ungureanu@bearingpoint.com>
2019-05-04 14:37:51 +0300

    changed package js

--- DB.js
+++ DB.js
@@ -1,3 +1,6 @@
 module.exports = {
-    DB: 'mongodb://ungureanu:Botosani1989@ds050539.mlab.com:50539/bogdaprosteapp'
+    DB: 'mongodb+srv://ungureanu:Botosani1989!1@cluster0-t0frr.mongodb.net/test?retryWrites=true'
 };
+
+// mongodb://ungureanu:Botosani1989@ds050539.mlab.com:50539/bogdaprosteapp
+// mongodb+srv://ungureanu:Botosani1989!1@cluster0-t0frr.mongodb.net/test?retryWrites=true
