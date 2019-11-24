16258c1993273b43b001fffd948d42fcfa36bad9
ungureanu_claus2@yahoo.com <ungureanu_claus2@yahoo.com>
2019-11-24 23:25:41 +0200

    updated model

--- models/Business.js
+++ models/Business.js
@@ -1,96 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Define collection and schema for Business
-let Business = new Schema({
-    brand_name: {
-        type: String
-    },
-    brand_description: {
-        type: String
-    },
-    user_email: {
-        type: String
-    },
-    user_phone: {
-        type: String
-    }
-}, {
-        collection: 'business'
-    });
-
-let Survey = new Schema({
-    quality: {
-        type: Object
-    },
-    email: {
-        type: String
-    },
-    price: {
-        type: String
-    },
-    price_to_competitors: {
-        type: String
-    },
-    price_limit: {
-        type: Object
-    },
-    satisfaction: {
-        type: Number
-    },
-    suggestions: {
-        type: String
-    }
-});
-
-let SurveySchema = new Schema({
-  sex: {
-      type: Number
-  },
-  age: {
-    type: String
-  },
-  firstName: {
-    type: String
-  },
-  lastName: {
-    type: String
-  },
-  phone: {
-    type: String
-  },
-  email: {
-    type: String
-  },
-  agreement: {
-    type: Number
-  },
-  date: {
-    type: Date
-  },
-}, {
-      collection: 'volunteers'
-  });
-
-  let ContentSchema = new Schema({
-    title: {
-        type: String
-    },
-    body: {
-      type: String
-    }
-  }, {
-        collection: 'content'
-    });
-
   let TimelineSchema = new Schema({
-    age: { type: String },
+    age: { type: Object },
     animalType: { type: String },
     title: { type: String },
     picture: { type: String },
     subtitle: { type: String },
     descriptionText: { type: String },
-    infoItems: { type: [String] }
+    infoItems: { type: [Object] },
+    timelineIndex: { type: String },
   }, {
         collection: 'timeline'
     });
--- routes/business.route.js
+++ routes/business.route.js
@@ -6,28 +6,20 @@ const businessRoutes = express.Router();
 // Require Business model in our routes module
 let Business = require('../models/Business');
 
-businessRoutes.route('/addContent').post(function (req, res) {
-  let business = new Business.Content(req.body);
-  business.save()
-      .then(business => {
-          res.status(200).json({ 'business': 'business in added successfully' });
-      })
-      .catch(err => {
-          res.status(400).send("unable to be saved in database" + err);
-      });
-});
-
-// Defined get data(index or listing) route
-businessRoutes.route('/api/timeline').get( async (req, res) => {
-    Business.Timeline.find({}, (err, item) => {
+// GET
+businessRoutes.route('/api/timeline/:type').get( async (req, res) => {
+    Business.Timeline.
+        find({}, (err, item) => {
             if (err) {
               res.status(500).send(err);
             }
             res.status(200).json(item);
-    });
+        }).
+        where('animalType').equals(req.params.type).
+        sort('timelineIndex');
 });
 
-//POST s
+//POST
 businessRoutes.route('/api/timeline/new').post(function (req, res) {
     let business = new Business.Timeline(req.body);
     business.save()
@@ -39,50 +31,31 @@ businessRoutes.route('/api/timeline/new').post(function (req, res) {
         });
 });
 
-businessRoutes.route('/getContent').get( async (req, res) => {
-  Business.Content.find({}, (err, user) => {
-          if (err) {
-            res.status(500).send(err);
-          }
-          res.status(200).json(user);
-  });
-});
-
-
-// Defined edit route
-businessRoutes.route('/edit/:id').get(function (req, res) {
-    let id = req.params.id;
-    Business.findById(id, function (err, business) {
-        res.json(business);
-    });
-});
-
-//  Defined update route
-businessRoutes.route('/update/:id').post(function (req, res) {
-    Business.findById(req.params.id, function (err, next, business) {
-        if (!business)
-            return next(new Error('Could not load Document'));
-        else {
-            business.person_name = req.body.person_name;
-            business.business_name = req.body.business_name;
-            business.business_gst_number = req.body.business_gst_number;
-
-            business.save().then(business => {
-                res.json('Update complete');
-            })
-                .catch(err => {
-                    res.status(400).send("unable to update the database");
-                });
-        }
-    });
-});
-
-// Defined delete | remove | destroy route
-businessRoutes.route('/delete/:id').get(function (req, res) {
-    Business.findByIdAndRemove({ _id: req.params.id }, function (err, business) {
-        if (err) res.json(err);
-        else res.json('Successfully removed');
-    });
-});
+// PUT
+businessRoutes.route('/api/timeline/edit/:id').put((req, res, next) => {
+    Business.Timeline.findByIdAndUpdate(req.params.id, {
+      $set: req.body
+    }, (error, data) => {
+      if (error) {
+        return next(error);
+        console.log(error)
+      } else {
+        res.json(data)
+        console.log('Data updated successfully')
+      }
+    })
+  })
+  
+// DELETE 
+businessRoutes.route('/api/timeline/delete/:id').delete((req, res, next) => {
+    Business.Timeline.findOneAndRemove({ _id: req.params.id }, (error, data) => {
+      if (error) {
+        return next(error);
+      } else {
+        res.status(200).json('deleted successfully')
+        console.log('Data deleted successfully')
+      }
+    })
+  })
 
 module.exports = businessRoutes;
