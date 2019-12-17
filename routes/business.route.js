5a729c93b8ca5c3a9ab46648717fd8ffdff91a78
ungureanu <ungureanu_claus2@yahoo.com>
2019-12-17 22:28:41 +0200

    saved to filesystem

--- routes/business.route.js
+++ routes/business.route.js
@@ -1,13 +1,47 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const businessRoutes = express.Router();
 
 let multer = require('multer');
-let upload = multer();
+// let upload = multer();
 
 // Require Business model in our routes module
 let Business = require('../models/Business');
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 10000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('picture');
+
+// Check File Type
+function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
 
 // GET
 businessRoutes.route('/api/timeline/:type').get( async (req, res) => {
@@ -23,25 +57,49 @@ businessRoutes.route('/api/timeline/:type').get( async (req, res) => {
 });
 
 //POST
-businessRoutes.route('/api/timeline/new').post(upload.single("foo-bar"), function (req, res) {
-    console.log( req.body );
-    console.log( req.files );
+businessRoutes.route('/api/timeline/new').post((req, res) => {
+    
+   
 
-    let business = new Business.Timeline(req.body);
-    if (!req.file) {
-      console.log("No file received");
-    } else {
-      console.log('file received');
-    }
+    upload(req, res, (err) => {
+      if(err){
+        // res.render('index', {
+        //   msg: err
+        // });
+        console.log(err);
+      } else {
+        if(req.file == undefined){
+          // res.render('index', {
+          //   msg: 'Error: No File Selected!'
+          // });
+          console.log('No File Selected');
+        } else {
+          console.log( req.file );
+          console.log( req.body );
+          // res.render('index', {
+          //   msg: 'File Uploaded!',
+          //   file: `uploads/${req.file.filename}`
+          // });
+        }
+      }
+    });
 
-    business.save()
-        .then(business => {
-            res.status(200).json({ 'timelineItem': 'added successfully' });
-        })
-        .catch(err => {
-            res.status(400).send("unable to be saved in database" + err);
-        });
+    // let business = new Business.Timeline(req.body);
+    // if (!req.file) {
+    //   console.log("No file received");
+    // } else {
+    //   console.log('file received');
+    // }
+
+    // business.save()
+    //     .then(business => {
+    //         res.status(200).json({ 'timelineItem': 'added successfully' });
+    //     })
+    //     .catch(err => {
+    //         res.status(400).send("unable to be saved in database" + err);
+    //     });
 });
+
 
 // PUT
 businessRoutes.route('/api/timeline/edit/:id').put((req, res, next) => {
